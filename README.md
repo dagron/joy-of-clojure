747aeacd382380a072fbc8670d5ae7be5f3c3675
Daniel Miladinov <daniel.miladinov@gmail.com>
2014-12-21 11:41:40 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# joy-of-clojure
+
+This repo tracks my progress as I work through Joy of Clojure (2nd Ed.)
+
+## License
+
+Copyright © 2014 Daniel Miladinov
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject app "0.1.0"
+  :description "My Learnings as I work through Joy of Clojure 2nd Ed."
+  :url "https://github.com/danielmiladinov/joy-of-clojure"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])

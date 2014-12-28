9f5de6eda2da950d08c9bd9f86e3af497bd91db7
Daniel Miladinov <daniel.miladinov@gmail.com>
2014-12-28 10:49:54 -0500

    Add examples of unquote-splicing

---
+++ src/joy-of-clojure/chapter2/unquote_splicing.clj
@@ -0,0 +1,16 @@
+;; Unquote-splicing
+;; ---------------------------------------------------------------------------------------------------------------------
+;; Clojure provides a handy feature to solve exactly the problem posed earlier. A variant of unquote called
+;; unquote-splicing works similarly to unquote, but a little differently:
+(let [x '(2 3)] `(1 ~@x))
+;;=> (1 2 3)
+
+;; Note the @ in ~@, which tells Clojure to unpack the sequence x, splicing it into the resulting list rather than
+;; inserting it as a nested list.
+
+;; Note that unquote-splicing does the right thing even when the container is a different type:
+(let [x '(3 4)] `[1 2 ~@x])
+;;=> [1 2 3 4]
+
+(let [x [:c :d]] `(:a :b ~@x))
+;;=> (:a :b :c :d)

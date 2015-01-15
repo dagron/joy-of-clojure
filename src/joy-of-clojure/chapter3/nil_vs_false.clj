63e5886deabde0aac6bf64398f16a5ed5a11f4b8
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-01-14 21:47:57 -0500

    Add examples of nil vs false

---
+++ src/joy-of-clojure/chapter3/nil_vs_false.clj
@@ -0,0 +1,12 @@
+;; Nil vs false
+;; ---------------------------------------------------------------------------------------------------------------------
+;; Rarely do you need to differentiate between the two non-truthy values, but if you do, you can use nil? and false?:
+(when (nil? nil) "Actually nil, not false")
+;;=> "Actually nil, not false"
+
+(when (false? false) "Actually false, not nil")
+;;=> "Actually false, not nil"
+
+;; Keeping in mind the basic rule that everything in Clojure is truthy unless it's false or nil is an astonishingly
+;; powerful concept, allowing for elegant solutions. Often pro- gramming languages have complicated semantics for
+;; truthiness, but Clojure manages to avoid those matters nicely.

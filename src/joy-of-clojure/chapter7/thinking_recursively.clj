c75318828386e904d85e7e717542097b7d975b8b
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-04-21 20:45:06 -0400

    Add discussion on mundane recursion

--- src/joy-of-clojure/chapter7/thinking_recursively.clj
+++ src/joy-of-clojure/chapter7/thinking_recursively.clj
@@ -9,3 +9,42 @@
 ;; who've attained some level of acumen with functional programming, finding a recursive solution still injects a bit of
 ;; magic into our day. Recursion is a perfect building block for creating higher-level looping constructs and functions,
 ;; as we'll show in this section.
+
+;; Mundane recursion
+;; A classically recursive algorithm is that of calculating some base number raised to an exponent, or the pow function.
+;; A straightforward way to solve this problem recursively is to multiply the base by each successively smaller value of
+;; the exponent, as implemented here:
+(defn pow [base exp]
+  (if (zero? exp)
+    1
+    (* base (pow base (dec exp)))))
+
+(pow 2 10)
+;;=> 1024
+
+(pow 1.01 925)
+;;=> 9937.353723241924
+
+;; We say that the recursive call is mundane because it's named explicitly rather than through mutual recursion or
+;; implicitly with the recur special form. Why is this a problem? The answer lies in what happens when you try to call
+;; pow with a large value:
+(pow 2 10000)
+;;=> java.lang.StackOverflowError
+
+;; The implementation of pow is doomed to throw java.lang.StackOverflowError because the recursive call is trapped by
+;; the multiplication operation. The ideal solution is a tail-recursive version that uses the explicit recur form, thus
+;; avoiding stack consumption and the resulting exception. One way to remove the mundane recursive call is to perform
+;; the multiplication at a different point, thus freeing the recursive call to occur in the tail position, as shown next:
+(defn pow [base exp]
+  (letfn [(kapow [base exp acc]
+                 (if (zero? exp)
+                   acc
+                   (recur base (dec exp) (* base acc))))]
+    (kapow base exp 1)))
+(pow 2N 10000)
+;;=> ... a ridiculously large number
+
+;; This new version of pow uses two common techniques for converting mundane recursion to tail recursion. First, it uses
+;; a helper function kapow that does the majority of the work. Second, kapow uses an accumulator acc that holds the
+;; result of the multiplication. The exponent exp is no longer used as a multiplicative value but instead functions as a
+;; decrementing counter, eliminating a stack explosion.

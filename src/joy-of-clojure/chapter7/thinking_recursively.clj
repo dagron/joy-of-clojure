067e6c9d8b3b4bc2537141481067efa5d1564854
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-04-21 20:44:32 -0400

    Start a new section on thinking recursively

---
+++ src/joy-of-clojure/chapter7/thinking_recursively.clj
@@ -0,0 +1,11 @@
+;; Thinking recursively
+;; ---------------------------------------------------------------------------------------------------------------------
+;; You're likely already familiar with the basics of recursion, and as a result you can take heart that we won't force
+;; you to read a beginner's tutorial again. But because recursive solutions are prevalent in Clojure code, it's
+;; important for us to cover it well enough that you can fully understand Clojure's recursive offerings.
+
+;; Recursion is often viewed as a low-level operation reserved for times when solutions involving higher-order functions
+;; either fail or lead to obfuscation. Granted, it's fun to solve problems recursively because even for those of us
+;; who've attained some level of acumen with functional programming, finding a recursive solution still injects a bit of
+;; magic into our day. Recursion is a perfect building block for creating higher-level looping constructs and functions,
+;; as we'll show in this section.
